71b9e3aea16360e3877d847cb41f47b12c7a26ff
bprecia1 <107278642+bprecia1@users.noreply.github.com>
2024-02-20 18:20:06 -0800

    editing

--- src/router.js
+++ src/router.js
@@ -1,18 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/views/HomePage.vue'
-import CardDetails from '@/views/CardDetails.vue'
+import CardDetails from './views/CardDetails.vue'
 import LoginPage from './views/LoginPage.vue'
-import SettingsPage from './views.SettingsPage.vue'
+import SettingsPage from './views/SettingsPage.vue'
 
 import { useAuth } from './composables/useAuth'
-const {isAuthenticated}= useAuth()
+const {isAuthenticated} = useAuth()
+
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
-  { path: '/employees/:id', name: 'CardDetails', component: CardDetails },
-  { path: '/login', name: 'LoginPage', component: LoginPage },
-  { path: '/settings', name: 'SettingsPage', component: SettingsPage },
+  { path: '/company-directory/', name: 'Home', component: HomePage },
+  { path: '/company-directory/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/company-directory/employees/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/company-directory/login', name: 'LoginPage', component: LoginPage },
+  { path: '/company-directory/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
 ]
 
 const router = createRouter({
